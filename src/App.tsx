8ee4e1db24dff43ae2406acc0b05764f59959f28
Kris1027 <obarzanek.work@gmail.com>
2023-10-25 00:09:19 +0200

    build todo functions

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,32 @@
-function App() {
-  return <div>elo</div>;
-}
+import { useState } from 'react';
+
+import { Todo } from './todo.models';
+
+import NewTodo from './components/NewTodo';
+import TodoList from './components/TodoList';
+
+const App: React.FC = () => {
+  const [todos, setTodos] = useState<Todo[]>([]);
+
+  const todoAddHandler = (text: string) => {
+    setTodos((prevTodos) => [
+      ...prevTodos,
+      { id: Math.random().toString(), text: text },
+    ]);
+  };
+
+  const todoDeleteHandler = (todoId: string) => {
+    setTodos((prevTodos) => {
+      return prevTodos.filter((todo) => todo.id !== todoId);
+    });
+  };
+
+  return (
+    <>
+      <NewTodo onAddTodo={todoAddHandler} />
+      <TodoList items={todos} onDeleteTodo={todoDeleteHandler} />
+    </>
+  );
+};
 
 export default App;
